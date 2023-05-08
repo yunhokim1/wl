cde5e4072c1c8291c44be77227ccc3cb7ef39f86
yunhokim1 <114965423+yunhokim1@users.noreply.github.com>
2023-05-08 21:03:35 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -13,12 +13,13 @@ function login(){
         psword: psword.value,
     };
 
-fetch("/login", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(req)
-})
+    fetch("/login", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    }).then((res) => res.json())
+      .then(console.log);
 
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,43 @@
 "use strict";
 
-const hello = (req, res) => {
-    res.render("home/index");
-}
 
-const login = (req,res) => {
-    res.render("home/login");
-}
+const output = {
+    hello: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req,res) => {
+        res.render("home/login");
+    },
+};
+
+const users = {
+    id: ["woorimIT", "나개발", "김팀장"],
+    psword: ["1234", "1234", "123456"],
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        if (users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+        
+    },
+};
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process,
 };
